e0bdc5f4884aa95c62be6e2bf25a5529740d844b
barlik123 <barlik123@cs.huji.ac.il>
2024-03-30 21:43:48 +0300

    readability and move_range function

--- Untitled-1.py
+++ Untitled-1.py
@@ -19,30 +19,39 @@ def print_board(board: BOARD_TYPE) ->None:
 
 print_board(Board1)
 
-#gets a board and two cordinates and updates cord1 to empty
-#(O) and cord2 on the board to having a piece (X)
-def update_piece(cord1: Tuple[int,int], cord2: Tuple[int,int],
+#gets a board and two cordinates and updates orig_cord to empty
+#(O) and dest_cord on the board to having a piece (X)
+def update_piece(orig_cord: Tuple[int,int], dest_cord: Tuple[int,int],
                 board: BOARD_TYPE):
-    board[cord1[0]][cord1[1]] = 'O'
-    board[cord2[0]][cord2[1]] = 'X'
+    board[orig_cord[0]][orig_cord[1]] = 'O'
+    board[dest_cord[0]][dest_cord[1]] = 'X'
 
 #function that checks if a piece can move from cord1 to cord2
-#on the board. 
+#on the board. (if there is a piece in the selected location
+# and no piece in the destination location)
 #the function returns a boolean.
-def can_move(cord1: Tuple[int,int], cord2: Tuple[int,int],
+def can_move(orig_cord: Tuple[int,int], dest_cord: Tuple[int,int],
                 board: BOARD_TYPE) ->bool:
-    return board[cord1[0]][cord1[1]]=='X' and board[cord2[0]][cord2[1]]=='O'
+    return board[orig_cord[0]][orig_cord[1]]=='X' and board[dest_cord[0]][dest_cord[1]]=='O'
 
 #function that executes the movement logic of the piece.
 #gets the original and new cordinates and board,
 #checks if the piece can move to the new cordinates
 #and moves it there.
-def move_piece(cord1: Tuple[int,int], cord2: Tuple[int,int],
+def move_piece(orig_cord: Tuple[int,int], dest_cord: Tuple[int,int],
                 board: BOARD_TYPE) -> None:
-    if can_move(cord1, cord2, Board1):
-        update_piece(cord1, cord2, Board1)
+    if can_move(orig_cord, dest_cord, Board1) and move_range(orig_cord, dest_cord):
+        update_piece(orig_cord, dest_cord, Board1)
 
-move_piece((3,3),(5,6),Board1)
-move_piece((1,7),(2,2),Board1)
+#function that checks if the destination cordinates of a move
+#are withing one diagonal square from the origin cordinate
+#and returns a boolean 
+def move_range(orig_cord: Tuple[int,int], dest_cord: Tuple[int,int]) -> bool:
+    return (dest_cord[0] == orig_cord[0] - 1 and 
+        (dest_cord[1] == orig_cord[1] + 1 or dest_cord[1] == orig_cord[1] -1))
+        
+move_piece((5,1),(4,0),Board1)
+move_piece((5,5),(4,6),Board1)
+move_piece((4,0),(3,1),Board1)
 print('\n')
 print_board(Board1)
